5d2404c6508e4991d06edf111951def4d8ccb3bf
skalyan11 <skalyan@umich.edu>
2024-07-29 14:06:11 -0500

    herosection without image

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-
 const HeroSection = () => {
-  return (
-    <div className="flex flex-col items-center mt-6 lg:mt-20">
+    return (
+      <div className="flex flex-col items-center mt-6 lg:mt-20">
         <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
-            Places 
-        <span className="bg-gradient-to-r from-green-500 to-green-800 text-transparent bg-clip-text">
-            {" "} at Your Disposal
-        </span>
+          <span className="bg-gradient-to-r from-green-500 to-green-800 text-transparent bg-clip-text">
+            Places
+          </span>
+          {" "} at Your Disposal
         </h1>
         <p className="mt-10 text-lg text-center text-neutral-500">
-            Having trouble choosing a place to eat? Start here
+          Having trouble choosing a place to eat? Start here
         </p>
         <div className="flex justify-center my-10">
-            <a href="#" className="bg-gradient-to-r from-cyan-500 to-cyan-800 py-3 px-4 mx-3 rounded-md">
-                Get Started Now
-            </a>
+          <a href="#" className="bg-gradient-to-r from-cyan-100 to-cyan-300 py-3 px-4 mx-3 rounded-md">
+            Get Started Now
+          </a>
+          <a href="#" className="py-3 px-4 mx-3 rounded-md border">
+            Learn More
+          </a>
         </div>
-    </div>
-  );
-};
-
-export default HeroSection;
+      </div>
+    );
+  };
+  
+  export default HeroSection;
