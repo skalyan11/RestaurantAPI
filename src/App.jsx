3bf07dcd611b5d9212a2a988edec44f0884ae062
asrith132 <asrithrn@gmail.com>
2024-08-03 23:02:19 -0500

    LoginSignUp page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import SignInPage from './components/SignInPage';
 import CreateAccountPage from './components/CreateAccountPage';
+import LoginSignUp from './components/LoginSignUp/LoginSignUp';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signin" element={<LoginSignUp />} />
         <Route path="/create-account" element={<CreateAccountPage />} />
         {/* Add other routes as needed */}
       </Routes>
